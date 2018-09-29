6890280ebc6bd062f71cae30fcaeeec76def71c4
vvusu <wedxzl@gmail.com>
2018-09-29 11:01:56 +0800

    update code

--- build/LNRefresh.build/Debug-iphoneos/LNRefresh-Resources.build/Objects-normal/armv7/LNRefresh_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vvusu/Downloads/LNRefresh/build/LNRefresh.build/Debug-iphoneos/LNRefresh-Resources.build/DerivedSources/LNRefresh_vers.c
--- build/LNRefresh.build/Debug-iphonesimulator/LNRefresh-Resources.build/Objects-normal/i386/LNRefresh_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vvusu/Downloads/LNRefresh/build/LNRefresh.build/Debug-iphonesimulator/LNRefresh-Resources.build/DerivedSources/LNRefresh_vers.c
