0fc52cf599c21c0b7fa1bb51a3e366ff5566d484
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-28 20:42:32 +0800

    Delete build directory

--- build/LNRefresh.build/Debug-iphoneos/LNRefresh-Resources.build/Objects-normal/armv7/LNRefresh_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vvusu/Work/iOS/LNRefresh/build/LNRefresh.build/Debug-iphoneos/LNRefresh-Resources.build/DerivedSources/LNRefresh_vers.c
--- build/LNRefresh.build/Debug-iphonesimulator/LNRefresh.build/Objects-normal/x86_64/LNHeaderAnimator.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNHeaderAnimator.m \
-  /Users/vvusu/Work/iOS/LNRefresh/build/LNRefresh.build/Debug-iphonesimulator/LNRefresh.build/module.modulemap \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNHeaderAnimator.h \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNRefreshAnimator.h \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNRefreshConst.h \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNRefreshHandler.h
