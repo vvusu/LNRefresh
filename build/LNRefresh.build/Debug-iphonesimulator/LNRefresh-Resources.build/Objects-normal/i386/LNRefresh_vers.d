241f12e3dcd4813cbff451d591fc75c308ce0148
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-28 20:41:44 +0800

    Delete build directory

--- build/LNRefresh.build/Debug-iphonesimulator/LNRefresh-Resources.build/Objects-normal/i386/LNRefresh_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/vvusu/Work/iOS/LNRefresh/build/LNRefresh.build/Debug-iphonesimulator/LNRefresh-Resources.build/DerivedSources/LNRefresh_vers.c
--- build/LNRefresh.build/Debug-iphonesimulator/LNRefresh.build/Objects-normal/i386/LNRefreshConst.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNRefreshConst.m \
-  /Users/vvusu/Work/iOS/LNRefresh/build/LNRefresh.build/Debug-iphonesimulator/LNRefresh.build/module.modulemap \
-  /Users/vvusu/Work/iOS/LNRefresh/LNRefresh/LNRefresh/LNRefreshConst.h
